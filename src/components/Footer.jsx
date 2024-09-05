ef319fdc0a9dfd8686c8d4e1a75efbc59212ee25
anjali24srivastava <anjalisrivastava118@gmail.com>
2024-09-05 23:23:54 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='footer-container'>
+            
+            <p>Design and Developed by Anjali Srivastava </p>
+
+            <div className='logos'>
+                <a target='_blank' href="https://github.com/anjali24srivastava">
+                    <i className='bx bxl-github' ></i>
+                </a>
+
+                <a target='_blank' href="https://www.linkedin.com/in/anjali-srivastava-650b12231/">
+                    <i className='bx bxl-linkedin-square' ></i>
+                </a>
+            </div>
+
+            
+        </footer>
+    )
+}
+
+export default Footer
